f984e48f81d5469d4de73d5929dfa070508596fb
Piotr Kluz <piox89@gmail.com>
2020-06-23 00:08:23 +0200

    Add Simple logger for RestAssured

---
+++ src/test/groovy/io/github/piotrkluz/config/SimpleLogFilter.groovy
@@ -0,0 +1,45 @@
+package io.github.piotrkluz.config
+
+import io.restassured.filter.Filter
+import io.restassured.filter.FilterContext
+import io.restassured.filter.log.UrlDecoder
+import io.restassured.response.Response
+import io.restassured.specification.FilterableRequestSpecification
+import io.restassured.specification.FilterableResponseSpecification
+
+import java.nio.charset.Charset
+import java.util.logging.Logger
+
+class SimpleLogFilter implements Filter {
+    private final static LIMIT_LENGTH = 2000
+
+    @Override
+    Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
+        // Log Request
+        String uri = prepareUri(requestSpec)
+        String requestBody = prepareBody(requestSpec.getBody())
+        String method = requestSpec.getMethod()
+        System.out.println("\n$method $uri $requestBody")
+
+        // Log response
+        Response response = ctx.next(requestSpec, responseSpec)
+        String responseBody = prepareBody(response.getBody().asString())
+        System.out.println("RESPONSE ${response.statusCode} [${response.getTime()}ms] $responseBody")
+
+        return response
+    }
+
+    private static String prepareUri(FilterableRequestSpecification requestSpec) {
+        return UrlDecoder.urlDecode(
+                requestSpec.getURI(),
+                Charset.forName(requestSpec.getConfig().getEncoderConfig().defaultQueryParameterCharset()),
+                true
+        )
+    }
+
+    private static String prepareBody(String body) {
+        body == null ? "" : body.length() > 2000
+                ? body.substring(0, LIMIT_LENGTH)
+                : body
+    }
+}
