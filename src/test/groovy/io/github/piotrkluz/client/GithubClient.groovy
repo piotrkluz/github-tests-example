e4932ec323477a7b9a20e2c8ec4fbe1ff7d94aad
Piotr Kluz <piox89@gmail.com>
2020-06-23 18:43:57 +0200

    Use configuration managment library: lightbend.config

--- src/test/groovy/io/github/piotrkluz/client/GithubClient.groovy
+++ src/test/groovy/io/github/piotrkluz/client/GithubClient.groovy
@@ -1,17 +1,28 @@
 package io.github.piotrkluz.client
 
-import io.github.piotrkluz.config.Config
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
 import io.restassured.RestAssured
 import io.restassured.specification.RequestSpecification
 
 abstract class GithubClient {
-    protected String baseUrl = Config.githubApiUrl()
-    private String token = Config.accessToken()
+    private Config conf = loadConfig()
+    private String baseUrl = conf.getString("prod.githubApiUrl")
+    private String token = conf.getString("prod.githubToken")
 
     RequestSpecification request() {
         return RestAssured.given()
                 .baseUri(baseUrl)
                 .header("Authorization", "token $token")
                 .header("content-type", "application/json")
+    }
+
+    private static loadConfig() {
+        Config conf = ConfigFactory.load()
+        if(conf.getString("prod.githubToken").isEmpty()) {
+            throw new RuntimeException("Please set Github Access token in resources/application.conf")
+        }
+
+        return conf
     }
 }
--- src/test/groovy/io/github/piotrkluz/config/ConfigModel.groovy
+++
@@ -1,9 +0,0 @@
-package io.github.piotrkluz.config
-
-import groovy.transform.Canonical
-
-@Canonical
-class ConfigModel {
-    String githubApiUrl
-    String githubToken
-}
